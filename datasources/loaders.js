8d8b022c6b09cba8fb3bc27ad264f217f219b310
Santiago Calvo <sacalvo@cisco.com>
2021-11-12 10:28:18 -0300

    comment on loader

--- datasources/loaders.js
+++ datasources/loaders.js
@@ -1,5 +1,6 @@
 const DataLoader = require("dataloader");
 
+// Esto no va a escalar muy bien, capaz una especie de factory para los loader seria lo correcto, pero por ahora está bien.
 const rocketsLoader = (api) => {
   const loader = new DataLoader(ids => api.getRocketsByIds(ids));
   return () => loader;
